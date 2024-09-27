244c4acc83eba393681cb3ad5d6bbf821524eeb5
Vu Hong Quan (BRT-VN) <quan.vu@brtchip.com>
2024-09-27 12:32:05 +0700

    Almost OK

--- src/App.svelte
+++ src/App.svelte
@@ -33,17 +33,25 @@
     const response = await fetch(API_URL);
     const data = await response.json();
     if (data.length > 0) {
-      portfolioData = data;
+      portfolioData = data.map(crypto => ({
+        ...crypto,
+        current_price: crypto.current_price,
+        price_change_percentage_24h: crypto.price_change_percentage_24h,
+        image: crypto.image
+      }));
     }
   });
 </script>
 
 <main>
   <ul style="list-style-type: none; text-align: left;">
-    <li style="display: flex; justify-content: space-between; font-weight: bold;">Name <span style="text-align: right;">Value</span></li>
+    <li style="display: flex; justify-content: space-between; font-weight: bold;">Name <span style="text-align: right;">Value</span><span style="text-align: right;">Change (24h)</span></li>
     <hr style="border-bottom: 2px solid black;">
     {#each portfolioData as crypto}
-      <li style="display: flex; justify-content: space-between;">{crypto.symbol} <span style="text-align: right;">{crypto.current_price}</span></li>
+      <li style="display: flex; justify-content: space-between; align-items: center;">
+        <img src={crypto.image} alt={crypto.symbol} style="width: 25px; height: 25px; margin-right: 10px;"> <!-- Image size reduced to 50% -->
+        {crypto.symbol} <span style="text-align: right;">{crypto.current_price}</span><span style="text-align: right;">{crypto.price_change_percentage_24h.toFixed(2)}%</span>
+      </li>
       <hr>
     {/each}
   </ul>
