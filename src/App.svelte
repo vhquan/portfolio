4dbbdb230c7b38a1891347c2d6c347664fca2fff
Vu Hong Quan (BRT-VN) <quan.vu@brtchip.com>
2024-09-27 12:24:27 +0700

    Call at once

--- src/App.svelte
+++ src/App.svelte
@@ -26,13 +26,13 @@
     "threshold-network-token",
     "worldcoin-wld"
   ];
-  let API_URL = `https://api.coingecko.com/api/v3/simple/price?ids=${cryptoIds.join(',')}&vs_currencies=usd`;
-  let portfolioData = {};
+  let API_URL = `https://api.coingecko.com/api/v3/coins/markets?ids=${cryptoIds.join(',')}&vs_currency=usd`;
+  let portfolioData = [];
 
   onMount(async () => {
     const response = await fetch(API_URL);
     const data = await response.json();
-    if (Object.keys(data).length > 0) {
+    if (data.length > 0) {
       portfolioData = data;
     }
   });
@@ -42,8 +42,8 @@
   <ul style="list-style-type: none; text-align: left;">
     <li style="display: flex; justify-content: space-between; font-weight: bold;">Name <span style="text-align: right;">Value</span></li>
     <hr style="border-bottom: 2px solid black;">
-    {#each Object.entries(portfolioData) as [crypto, { usd }]}
-      <li style="display: flex; justify-content: space-between;">{crypto} <span style="text-align: right;">{usd}</span></li>
+    {#each portfolioData as crypto}
+      <li style="display: flex; justify-content: space-between;">{crypto.symbol} <span style="text-align: right;">{crypto.current_price}</span></li>
       <hr>
     {/each}
   </ul>
