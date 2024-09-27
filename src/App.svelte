09ded753a1877eb4e44768880936b2e9227ee414
Vu Hong Quan (BRT-VN) <quan.vu@brtchip.com>
2024-09-27 12:04:02 +0700

    Update new logo, title and UI

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,5 @@
 <script>
   import { onMount } from 'svelte';
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
 
   let cryptoIds = [
     "automata",
@@ -41,37 +39,17 @@
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  {#each Object.entries(portfolioData) as [crypto, { usd }]}
-    <p>{crypto} -> {usd}</p>
-  {/each}
+  <ul style="list-style-type: none; text-align: left;">
+    {#each Object.entries(portfolioData) as [crypto, { usd }]}
+      <li style="display: flex; justify-content: space-between;">{crypto} <span style="text-align: right;">{usd}</span></li>
+      <hr>
+    {/each}
+  </ul>
 
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+  main {
+    padding: 1rem;
   }
 </style>
